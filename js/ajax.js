54b9ca5cdd79054bdfd1a3be579bcf37c9b55d75
Eric Zorn <goldencatch93@gmail.com>
2017-08-01 16:17:51 -0400

    Started Building Table

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,30 @@
 /* Eric Zorn: July 31, 2017 / AJAX and Voting for the President, Module 6: Homework 1 JS */
 
+
 //HTTP Requests and Sends
-var xmlHTTP = new XMLHttpRequest();
+var xmlHTTP = new XMLHttpRequest(); //Set up Request
+
+xmlHTTP.onreadystatechange = function() {
+    if (xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
+        // console.log(xmlHTTP.responseText);
+        var tableInfo = JSON.parse(xmlHTTP.responseText);
+        var tableHeading  = tableInfo.tableHeading;
+        var tablePresidents   = tableInfo.tableCellsPres;
+        var tableCellsBirth   = tableInfo.tableCellsBirthday;
+
+    }
+};
+
+// Set up the request
 xmlHTTP.open("GET", "http://schwartzcomputer.com/ICT4570/Resources/USPresidents.json");
+// Perform the request
 xmlHTTP.send();
-xmlHTTP.addEventListener('load', reqListener);
 
 
 
-function getId(id) {
-    return document.getElementById(id);
-}
 
-function getTagName(tag) {
-    return document.getElementsByTagName(tag);
-}
 
-function reqListener() {
-    console.log(this.responseText);
-}
+
+
+
+
