102142fa53fae7b26c03bb3dec2efe190a57a5a0
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-08-02 12:27:55 -0400

    Creating Table Head and Rows

--- js/ajax.js
+++ js/ajax.js
@@ -5,20 +5,49 @@
 var xmlHTTP = new XMLHttpRequest(); //Set up Request
 
 xmlHTTP.onreadystatechange = function() {
+    "use strict";
     if (xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
-        // console.log(xmlHTTP.responseText);
-        var tableInfo = JSON.parse(xmlHTTP.responseText);
-        var tableHeading  = tableInfo.tableHeading;
-        var tablePresidents   = tableInfo.tableCellsPres;
-        var tableCellsBirth   = tableInfo.tableCellsBirthday;
+        var dataJSON = xmlHTTP.responseText;
+        console.log(dataJSON);
+        var tableInfoParse = JSON.parse(dataJSON);
+
+        console.log(tableInfoParse);
+
+
+        function createTable() {
+            "use strict";
+            var table = document.getElementById("presidentTable");
+            var row = table.insertRow();
+            var head = table.createTHead();
+
+
+            for (var i = 0; i < presidents.president.name.length; i += 1) {
+                
+            }
+
+
+        }
+
+        createTable();
+
 
     }
 };
+
+
+
 
 // Set up the request
 xmlHTTP.open("GET", "http://schwartzcomputer.com/ICT4570/Resources/USPresidents.json");
 // Perform the request
 xmlHTTP.send();
+
+
+
+
+
+
+
 
 
 
